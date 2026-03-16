5676244d5a1b55ad4f466a5c34c83be6d4e7fe7d
Yves ARDELLIER <ardelliery@free.fr>
2026-03-16 21:24:32 +0100

    Montée de version de webapp pour déploiement

--- webapp/sw.js
+++ webapp/sw.js
@@ -1,5 +1,5 @@
 /* --- CONFIGURATION DU SERVICE WORKER --- */
-const CACHE_NAME = 'genealogie-mobile-v3.1.1';
+const CACHE_NAME = 'genealogie-mobile-v3.1.2';
 
 // Liste exhaustive des ressources à mettre en cache
 const ASSETS_TO_CACHE = [
